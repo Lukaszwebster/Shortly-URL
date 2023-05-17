c7db7c458701767ca7233eb88f70ad4e5226b6bf
Łukasz Samcik <l.samcik@gmail.com>
2023-05-17 10:49:15 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["index.html"],
+	theme: {
+		extend: {
+			colors: {
+				cyan: "hsl(180, 66%, 49%)",
+				darkViolet: "hsl(257, 27%, 26%)",
+				gray: "hsl(0, 0%, 75%)",
+				grayishViolet: "hsl(257, 7%, 63%)",
+				veryDarkBlue: "hsl(255, 11%, 22%)",
+				veryDarkViolet: "hsl(260, 8%, 14%)",
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
